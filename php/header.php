93c1be435750d249b1e7b44416652c48011a4016
c.dehondt <c.dehondt@live.com>
2017-09-07 13:12:56 +0200

    ajout fichier du projet E-commerce

---
+++ php/header.php
@@ -0,0 +1,41 @@
+<!doctype html>
+<html class="no-js" lang="">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="x-ua-compatible" content="ie=edge">
+        <title></title>
+        <meta name="description" content="">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <link rel="apple-touch-icon" href="apple-touch-icon.png">
+        <!-- Place favicon.ico in the root directory -->
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.100.2/css/materialize.min.css">
+
+        <link rel="stylesheet" href="css/normalize.css">
+        <link rel="stylesheet" href="css/main.css">
+        <script src="js/vendor/modernizr-2.8.3.min.js"></script>
+    </head>
+    <body>
+      <header>
+        <nav class="nav-extended card-panel teal light-blue darken-1">
+          <div class="nav-wrapper">
+            <img class="brand-logo" src="img/logo.png" alt="logo du site">
+            <a href="#" data-activates="mobile-demo" class="button-collapse"><i class="material-icons">menu</i></a>
+            <ul id="nav-mobile" class="right hide-on-med-and-down">
+              <li><a href="#"></a><i class="fa fa-facebook-square" aria-hidden="true"></i></li>
+              <li><a href="#"></a></li>
+              <li><a href="#"></a></li>
+            </ul>
+            <ul class="side-nav" id="mobile-demo">
+              <li><a href="sass.html">Sass</a></li>
+              <li><a href="badges.html">Components</a></li
+              <li><a href="collapsible.html">JavaScript</a></li>
+            </ul>
+          </div>
+          <div class="nav-content">
+
+          </div>
+        </nav>
+
+        <h1></h1>
+      </header>
