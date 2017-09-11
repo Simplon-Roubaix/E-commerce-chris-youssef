e46c7cf037449c1cfb078371ede71a817a369c43
c.dehondt <c.dehondt@live.com>
2017-09-11 11:30:53 +0200

    creation de la fiche détail

--- php/produits.php
+++ php/produits.php
@@ -9,27 +9,65 @@ $Produits=[
       ["titre"=> "Beauté", "image" => "img/image6.jpg", "resume"=>"Proin eu sapien elit. Aliquam iaculis ex a eros vestibulum aliquam. Pellentesque ac condimentum nibh, eget volutpat enim. Suspendisse potenti."]
     ];
 ?>
+
+<?php
+if (!isset($_POST['nom'])): ?>
 <section class="container row">
-  <h2>Nos produits</h2>
+
+<h2>Nos produits</h2>
 <?php
 
-    foreach ($Produits as $cle => $admin) {
+  foreach ($Produits as $cle => $valeur):?>
 
-      ?>
-
-    <div class="col s12 m4">
-      <div class="card">
-        <div class="card-image">
-          <img src= "<?php echo $admin['image'] ?>">
-          <a class="btn-floating halfway-fab waves-effect waves-light red"><i class="material-icons">add</i></a>
-        </div>
-        <div class="card-content">
-          <h3 class="card-title"><?php echo $admin["titre"] ?></h3>
-          <p><?php echo $admin["resume"]?></p>
-        </div>
+  <div class="col s12 m4">
+    <div class="card">
+      <div class="card-image">
+        <img src= "<?php echo $valeur['image'] ?>">
+        <form class=""  method="post">
+          <input type="hidden" name="nom" value="<?php echo $cle ?>">
+          <button class="btn-floating halfway-fab waves-effect waves-light red" type="submit"><i class="material-icons">add</i></button>
+        </form>
+      </div>
+      <div class="card-content">
+        <h3 class="card-title"><?php echo $valeur["titre"] ?></h3>
+        <p><?php echo $valeur["resume"] ?></p>
       </div>
     </div>
-<?php
-}
- ?>
+  </div>
+<?php endforeach;?>
 </section>
+<?php
+else : ?>
+
+<?php  $categorie= $_POST['nom'];
+?>
+
+<section class=" container row" >
+
+<h2><?php echo $Produits[$categorie]["titre"]?></h2>
+
+  <div class="col s12 m5">
+    <div class="card">
+
+      <div class="card-image">
+        <img src= "<?php echo $Produits[$categorie]['image'] ?>">
+        <form class="" action="produits.html" method="post">
+          <input type="hidden" name="nom" value="">
+          <a class="btn-floating halfway-fab waves-effect waves-light red" ><i class="material-icons">add</i></a>
+        </form>
+      </div>
+
+      <div class="card-content">
+        <h3 class="card-title"><?php echo $Produits[$categorie]["titre"] ?></h3>
+        <p><?php echo $Produits[$categorie]["Detail"] ?></p>
+      </div>
+
+      <div class="card-action">
+        <a class="waves-effect waves-light btn" type="submit" href="index.php">retour</a>
+      </div>
+
+    </div>
+  </div>
+</section>
+
+<?php endif; ?>
